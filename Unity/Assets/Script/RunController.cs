90294da630a6a1a7aac89f98b4887b4b7ad1d45f
kakkoiiowata <bt.shu.21@gmail.com>
2014-07-09 02:44:25 +0900

    UnityProject作成&テスト作成
    Unityちゃんを導入

---
+++ Unity/Assets/Script/RunController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunController : MonoBehaviour {
+
+	// 動作させるプレハブ
+	private GameObject moveBackGround;
+
+	void Start () {
+		// 動作するプレハブを設定する
+		var go = Resources.Load ("Prefab/BackGround/BackGround_Town_01");
+		moveBackGround = Instantiate (go) as GameObject;
+	}
+
+	void Update () {
+		if (moveBackGround != null) {
+			var position = moveBackGround.transform.position;
+			position.z -= 0.1f;
+			moveBackGround.transform.position = position;
+		}
+	}
+}
